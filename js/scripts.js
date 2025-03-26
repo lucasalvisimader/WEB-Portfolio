f00f8c2f9e99d4333df70c05d01020c88f5ccb13
Lucas <105462862+lucasalvisimader@users.noreply.github.com>
2025-03-26 14:33:39 -0300

    added projects and finished animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,34 @@
+// Gira a carta quando clicada
 const flipCard = (card) => {
     card.classList.toggle("flip")
 }
 
 // Executa o código quando a página carregar
 document.addEventListener("DOMContentLoaded", () => {
-    const sections = document.querySelectorAll("section");
-
-    sections.forEach((section, index) => {
-        // Define se a seção virá da esquerda ou da direita
-        const directionClass = index % 2 === 0 ? "from-left" : "from-right";
-        section.classList.add(directionClass);
-
-        // Adiciona a mesma classe nos elementos internos
-        const innerElements = section.querySelectorAll(".animate-item");
-        innerElements.forEach((item) => {
-            item.classList.add("hidden", directionClass);
-        });
-    });
-
+    const innerElements = document.querySelectorAll(".animate-item");
+    
+    // Define a animação da seção e dos itens da seção
     function animateOnScroll() {
+        const sections = document.querySelectorAll("section");
         sections.forEach(section => {
             const sectionPosition = section.getBoundingClientRect().top;
             const screenPosition = window.innerHeight / 1.3;
 
+            // Faz a animação da seção somente quando estiver sendo mostrada na tela
             if (sectionPosition < screenPosition) {
                 section.classList.add("animate");
 
                 // Ativar os elementos internos
-                const innerElements = section.querySelectorAll(".animate-item");
                 innerElements.forEach((item, i) => {
                     setTimeout(() => {
                         item.classList.add("fade-in");
-                    }, i * 500); // Delay entre elementos
+                    }, i * 400); // Delay entre elementos
                 });
             }
         });
     }
 
+    // Quando der scroll na página, a função é chamada novamente
     window.addEventListener("scroll", animateOnScroll);
     animateOnScroll();
 });
